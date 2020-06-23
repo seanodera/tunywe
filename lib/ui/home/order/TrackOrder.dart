2bc45cad221f8da4173e989300a38964d5fe1c8d
thegreatsoo421 <55226629+thegreatsoo421@users.noreply.github.com>
2020-06-23 04:45:01 +0300

    Android completed

---
+++ lib/ui/home/order/TrackOrder.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TrackOrder extends StatefulWidget {
+  @override
+  _TrackOrderState createState() => _TrackOrderState();
+}
+
+class _TrackOrderState extends State<TrackOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
